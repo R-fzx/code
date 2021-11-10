20bdf5e408c45cf0f2cf12b4113d27a62570a40d
wsfxk <79501232+R-fzx@users.noreply.github.com>
2021-11-10 17:57:25 +0800

    2021-11-10 17:57

--- template.cpp
+++ template.cpp
@@ -40,8 +40,8 @@ char buf[MAX_INPUT], *p1, *p2;
 template <typename T>
 inline int redi(T& x) {
   static std::streambuf* inbuf = cin.rdbuf();
-  register int f = x = 0, flag = 1;
-  register char ch = getc();
+  int f = x = 0, flag = 1;
+  char ch = getc();
   if (ch == EOF) return EOF;
   while (!std::isdigit(ch)) ch == '-' && (f = 1), ch = getc();
   if (std::isdigit(ch)) x = x * 10 + ch - '0', ch = getc(), flag = 0;
@@ -71,24 +71,6 @@ inline int redst(string& s) {
 }  // namespace IN
 namespace OUT {
 template <typename T>
-inline void put(T x) {
-  static std::streambuf* outbuf = cout.rdbuf();
-  static char stack[21];
-  static int top = 0;
-  if (x < 0) outbuf->sputc('-'), x = -x;
-  if (!x) {
-    outbuf->sputc('0'), outbuf->sputc('\n');
-    return;
-  }
-  while (x) stack[++top] = x % 10 + '0', x /= 10;
-  while (top) outbuf->sputc(stack[top]), --top;
-  outbuf->sputc('\n');
-}
-inline void putc(const char ch) {
-  static std::streambuf* outbuf = cout.rdbuf();
-  outbuf->sputc(ch);
-}
-template <typename T>
 inline void put_non(T x) {
   static std::streambuf* outbuf = cout.rdbuf();
   static char stack[21];
@@ -102,40 +84,21 @@ inline void put_non(T x) {
   while (top) outbuf->sputc(stack[top]), --top;
 }
 template <typename T>
-inline void put(const char ch, T x) {
+inline void put(const char ch, T x) { put_non(x), putc(ch); }
+template <typename T>
+inline void put(T x) { put('\n', x); }
+inline void putc(const char ch) {
   static std::streambuf* outbuf = cout.rdbuf();
-  static char stack[21];
-  static int top = 0;
-  if (x < 0) outbuf->sputc('-'), x = -x;
-  if (!x) {
-    outbuf->sputc('0'), outbuf->sputc(ch);
-    return;
-  }
-  while (x) stack[++top] = x % 10 + '0', x /= 10;
-  while (top) outbuf->sputc(stack[top]), --top;
   outbuf->sputc(ch);
 }
 inline void putst(const char* st) {
   static std::streambuf* outbuf = cout.rdbuf();
-  do {
-    outbuf->sputc(*(st++));
-  } while (*st);
+  do outbuf->sputc(*(st++));
+  while (*st);
 }
 inline void putst(string st) { putst(st.c_str()); }
 template <typename T>
-inline void put(const char* st, T x) {
-  static std::streambuf* outbuf = cout.rdbuf();
-  static char stack[21];
-  static int top = 0;
-  if (x < 0) outbuf->sputc('-'), x = -x;
-  if (!x) {
-    outbuf->sputc('0'), putst(st);
-    return;
-  }
-  while (x) stack[++top] = x % 10 + '0', x /= 10;
-  while (top) outbuf->sputc(stack[top]), --top;
-  putst(st);
-}
+inline void put(const char* st, T x) { put_non(x), putst(st); }
 template <typename T, typename... Args>
 inline void put(T a, Args... args) { put(a), put(args...); }
 template <typename T, typename... Args>
@@ -153,8 +116,7 @@ using OUT::putst;
 }  // namespace IO
 
 struct BigInt {
-  static const int BASE = 100000000;
-  static const int WIDTH = 8;
+  static const int BASE = 100000000, WIDTH = 8;
   vector<int> s;
   int len;
   BigInt& clean() {
@@ -163,127 +125,66 @@ struct BigInt {
   }
   BigInt(uLL num = 0) { *this = num; }
   BigInt(string s) { *this = s; }
+  // BigInt(char *s) { *this = string(s); }
   BigInt& operator=(long long num) {
     s.clear();
     long long t = num;
     while (t) t /= 10, len++;
-    do {
-      s.push_back(num % BASE);
-      num /= BASE;
-    } while (num > 0);
+    do s.push_back(num % BASE), num /= BASE;
+    while (num > 0);
     return *this;
   }
   BigInt& operator=(const string& str) {
     s.clear();
-    int x, l = (str.length() - 1) / WIDTH + 1;
-    len = str.length();
-    for (int i = 0; i < l; i++) {
-      int end = str.length() - i * WIDTH;
-      int start = max(0, end - WIDTH);
-      sscanf(str.substr(start, end - start).c_str(), "%d", &x);
-      s.push_back(x);
-    }
+    int x, l = ((len = str.length()) - 1) / WIDTH + 1;
+    for (int i = 0, end, start; i < l; i++) end = str.length() - i * WIDTH, start = max(0, end - WIDTH), sscanf(str.substr(start, end - start).c_str(), "%d", &x), s.push_back(x);
     return (*this).clean();
   }
   BigInt operator+(const BigInt& b) const {
     BigInt c;
     c.s.clear();
-    for (int i = 0, g = 0;; i++) {
-      if (g == 0 && i >= s.size() && i >= b.s.size()) break;
-      int x = g;
-      if (i < s.size()) x += s[i];
-      if (i < b.s.size()) x += b.s[i];
-      c.s.push_back(x % BASE);
-      g = x / BASE;
-    }
+    for (int i = 0, g = 0, x; g || i < s.size() || i < b.s.size(); i++) x = g, i < s.size() && (x += s[i]), i < b.s.size() && (x += b.s[i]), c.s.push_back(x % BASE), g = x / BASE;
     return c;
   }
   BigInt operator-(const BigInt& b) const {
     assert(b <= *this);
     BigInt c;
     c.s.clear();
-    for (int i = 0, g = 0;; i++) {
-      if (g == 0 && i >= s.size() && i >= b.s.size()) break;
-      int x = s[i] + g;
-      if (i < b.s.size()) x -= b.s[i];
-      if (x < 0) {
-        g = -1;
-        x += BASE;
-      } else
-        g = 0;
-      c.s.push_back(x);
-    }
+    for (int i = 0, g = 0, x; g || i < s.size() || i < b.s.size(); i++) x = s[i] + g, i < b.s.size() && (x -= b.s[i]), x < 0 ? (g = -1, x += BASE) : (g = 0), c.s.push_back(x);
     return c.clean();
   }
   BigInt operator*(const BigInt& b) const {
     int i, j;
-    uLL g;
+    uLL g, x;
     vector<uLL> v(s.size() + b.s.size(), 0);
     BigInt c;
     c.s.clear();
     for (i = 0; i < s.size(); i++)
-      for (j = 0; j < b.s.size(); j++)
-        v[i + j] += (uLL)(s[i]) * b.s[j];
-    for (i = 0, g = 0;; i++) {
-      if (g == 0 && i >= v.size()) break;
-      uLL x = v[i] + g;
-      c.s.push_back(x % BASE);
-      g = x / BASE;
-    }
+      for (j = 0; j < b.s.size(); j++) v[i + j] += (uLL)(s[i]) * b.s[j];
+    for (i = 0, g = 0; g || i < v.size(); i++) x = v[i] + g, c.s.push_back(x % BASE), g = x / BASE;
     return c.clean();
   }
   BigInt operator/(const BigInt& b) const {
     assert(b > 0);
     BigInt c = *this, m;
-    for (int i = s.size() - 1; i >= 0; i--) {
-      m = m * BASE + s[i];
-      c.s[i] = bsearch(b, m);
-      m -= b * c.s[i];
-    }
+    for (int i = s.size() - 1; i >= 0; i--) m = m * BASE + s[i], c.s[i] = bsearch(b, m), m -= b * c.s[i];
     return c.clean();
   }
   BigInt operator%(const BigInt& b) const {
     BigInt c = *this, m;
-    for (int i = s.size() - 1; i >= 0; i--) {
-      m = m * BASE + s[i];
-      c.s[i] = bsearch(b, m);
-      m -= b * c.s[i];
-    }
+    for (int i = s.size() - 1; i >= 0; i--) m = m * BASE + s[i], c.s[i] = bsearch(b, m), m -= b * c.s[i];
     return m;
   }
   int bsearch(const BigInt& b, const BigInt& m) const {
-    int L = 0, R = BASE - 1, x;
-    while (1) {
-      x = (L + R) >> 1;
-      if (b * x <= m) {
-        if (b * (x + 1) > m)
-          return x;
-        else
-          L = x;
-      } else
-        R = x;
-    }
+    int x;
+    for (int L = 0, R = BASE - 1; x = (L + R) >> 1, b * x > m || b * (x + 1) <= m; (b * x <= m ? L : R) = x) 1;
+    return x;
   }
-  BigInt& operator+=(const BigInt& b) {
-    *this = *this + b;
-    return *this;
-  }
-  BigInt& operator-=(const BigInt& b) {
-    *this = *this - b;
-    return *this;
-  }
-  BigInt& operator*=(const BigInt& b) {
-    *this = *this * b;
-    return *this;
-  }
-  BigInt& operator/=(const BigInt& b) {
-    *this = *this / b;
-    return *this;
-  }
-  BigInt& operator%=(const BigInt& b) {
-    *this = *this % b;
-    return *this;
-  }
+  BigInt& operator+=(const BigInt& b) { return *this = *this + b; }
+  BigInt& operator-=(const BigInt& b) { return *this = *this - b; }
+  BigInt& operator*=(const BigInt& b) { return *this = *this * b; }
+  BigInt& operator/=(const BigInt& b) { return *this = *this / b; }
+  BigInt& operator%=(const BigInt& b) { return *this = *this % b; }
   bool operator<(const BigInt& b) const {
     if (s.size() != b.s.size()) return s.size() < b.s.size();
     for (int i = s.size() - 1; i >= 0; i--)
@@ -299,8 +200,8 @@ struct BigInt {
     string _s;
     IO::redst(_s), *this = _s;
   }
-  void write() const {
-    IO::put(s.back());
+  void write() {
+    clean(), IO::put_non(s.back());
     for (int i = s.size() - 2; ~i; --i) {
       char sc[20];
       sprintf(sc, "%08d", s[i]), IO::putst(sc);
@@ -316,7 +217,7 @@ int main() {
 #endif
   ios_base::sync_with_stdio(0);
   cin.tie(0), cout.tie(0);
-
+  
 #ifdef TIME
   double t = 1.0 * clock() / CLOCKS_PER_SEC;
   cerr << "\n\nTIME: " << t << "s\n";
