00d1be011ddd577fa31b97aa438a99c2377157a3
R-fzx <3493508856@qq.com>
2022-10-09 19:11:43 +0800

    uiafhlchdchaidfso

--- contest/2022-10-03/source/fzx/sillyz/sillyz.cpp
+++ contest/2022-10-03/source/fzx/sillyz/sillyz.cpp
@@ -25,25 +25,44 @@ using LL = long long;
 using Pii = pair<int, int>;
 using Pll = pair<LL, LL>;
 
-const int kV = 1e7 + 1;
+LL a, b, ans;
+int c[10], _c[10], p[18], pc[18];
 
-LL a, b, c[kV];
+int S(LL n) { return log10(n) + 1; }
+void A(int x, int v) {
+  for (; x != 1; pc[p[x]] += v, x /= p[x]) {
+  }
+}
+LL C() {
+  int s = accumulate(_c + 1, _c + 10, 0);
+  for (int i = 1; i <= s; A(i++, 1)) {
+  }
+  for (int i = 1; i < 10; ++i) {
+    
+  }
+}
+void S(int l, LL n) {
+  
+}
+void D(int x, int l, LL n) {
+  if (l > S(n)) {
+    return;
+  }
+  if (x > 9) {
+    copy_n(c + 1, 9, _c + 1), S(l, n);
+    return;
+  }
+  D(x + 1, l, n), ++c[x], D(x, l + 1, n / x), --c[x];
+}
+LL C(LL n) {
+  ans = 0, D(1, 0, n);
+  return ans;
+}
 
 int main() {
   RF("sillyz");
   ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
   cin >> a >> b;
-  for (LL i = 1; i <= b; ++i) {
-    LL s = i;
-    for (LL j = i; j; s *= j % 10, j /= 10) {
-    }
-    if (s < kV) {
-      ++c[s];
-    }
-  }
-  for (LL i = 1; i <= b; ++i) {
-    c[i] += c[i - 1];
-  }
-  cout << c[b] - c[a - 1];
+  cout << C(b) - C(a - 1);
   return 0;
 }
